81127615731b0482192abff8c686f60d4d718ac4
khsagehorn <huntersagehorn@gmail.com>
2016-06-01 15:22:59 -0600

    browserify

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -13,8 +13,8 @@ renderer.resize(window.innerWidth, window.innerHeight);
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
+  //Create a container object called the `stage`
+  var stage = new PIXI.Container();
 
 //Tell the `renderer` to `render` the `stage`
 
@@ -25,41 +25,56 @@ PIXI.loader
   .add("images/foreground.png")
   .load(setup);
 
-  var sprite;
-  var token;
+  var state, sprite, token, background, foreground, titleScreen;
 
-  function setup() {
 
-    // Add the main srpite and tokens
-    sprite = new PIXI.Sprite(
-      PIXI.loader.resources["images/square_icon.png"].texture
-    );
-    token = new PIXI.Sprite(
-      PIXI.loader.resources["images/square_icon.png"].texture
-    );
+function setup() {
+  var height = 600;
 
-    // create the background and foreground
-    background = new PIXI.extras.TilingSprite(PIXI.loader.resources["images/background.png"].texture);
-    foreground = new PIXI.extras.TilingSprite(PIXI.loader.resources["images/foreground.png"].texture);
+  
+  // Create title screen
+  titleScreen = new PIXI.Container();
+  stage.addChild(titleScreen);
+  titleScreen.visible = true;
+  title = new PIXI.Text("Voxal", {font: "100px", fill: "white"});
+  title.x = 150;
+  title.y = 250;
+  titleScreen.addChild(title);
+
+  // Add the main srpite and tokens
+  sprite = new PIXI.Sprite(
+    PIXI.loader.resources["images/square_icon.png"].texture
+  );
+  token = new PIXI.Sprite(
+    PIXI.loader.resources["images/square_icon.png"].texture
+  );
+
+  // makeToken();
+
+  // create the background and foreground
+  background = new PIXI.extras.TilingSprite(PIXI.loader.resources["images/background.png"].texture);
+  foreground = new PIXI.extras.TilingSprite(PIXI.loader.resources["images/foreground.png"].texture);
    
 
-  // Position sprite
-    sprite.x = stage.width / 2;
-    token.x = 1200;
-    token.y = 0;
+  // Position sprites
+  sprite.x = 45;
+  sprite.y = height / 2;
+  console.log(stage.height);
+  token.x = 1200;
+  token.y = Math.floor(Math.random() *(renderer.height));
     // sprite.y = stage.height / 2;
 
   // Resize sprites
-    sprite.width = 200;
-    sprite.height = 200;
-    token.width = 75;
-    token.height = 75;
+  sprite.width = renderer.height/4;
+  sprite.height = renderer.height/4;
+  token.width = renderer.height/8;
+  token.height = renderer.height/8;
 
-  // Resize background
-  background.width = 1000;
-  foreground.width = 1000;
-  background.height = 600;
-  foreground.height = 600;
+  // Resize background/foreground
+  background.width = renderer.width;
+  foreground.width = renderer.width;
+  background.height = renderer.height;
+  foreground.height = renderer.height;
 
 
   // Sprite Velocity properties
@@ -68,19 +83,17 @@ PIXI.loader
   token.vx = 0;
   token.vy = 0;
 
-  // Rotate sprite
-    // sprite.rotation = 0.8;
-    // sprite.pivot.set(100, 100);
 
 
   // add sprite and render canvas
-   stage.addChild(background);
-   stage.addChild(foreground);
-    stage.addChild(sprite);
-    setInterval(function(){
-      newToken = token;
-      stage.addChild(newToken);
-    }, 1000);
+  stage.addChild(background);
+  stage.addChild(foreground);
+  stage.addChild(sprite);
+  // setInterval(function(){
+  //   newToken = token;
+  //   stage.addChild(newToken);
+  // }, 1000);
+  // makeToken();
       //Capture the keyboard arrow keys
   var left = keyboard(37),
       up = keyboard(38),
@@ -94,10 +107,8 @@ PIXI.loader
   };
   //Left arrow key `release` method
   left.release = function() {
-    //If the left arrow has been released, and the right arrow isn't down,
-    //and the sprite isn't moving vertically:
-    //Stop the sprite
-    if (!right.isDown && sprite.vy === 0) {
+  
+  if (!right.isDown && sprite.vy === 0) {
       sprite.vx = 0;
     }
   };
@@ -133,64 +144,72 @@ PIXI.loader
   };
 
     gameLoop();
+}
+
+function gameLoop(){
+  requestAnimationFrame(gameLoop);
+
+
+  // background movement
+  background.vx = -3 
+  background.tilePosition.x += background.vx;
+  foreground.vx = -5
+  foreground.tilePosition.x += foreground.vx;
+
+
+  makeToken();
+
+
+  // Update token velocity
+  token.vx = -4;
+
+
+
+  // Apply velocity values to position
+  sprite.x += sprite.vx;
+  sprite.y += sprite.vy;
+  token.x += token.vx;
+
+    //contain sprite in game window
+  // contain(sprite, {x:50, y:50, width: 1000, height: 600})
+
+  renderer.render(stage);
+  
+}
+
+function contain(sprite, container) {
+
+  var collision;
+
+  if (sprite.y < container.y) {
+    sprite.y = container.y;
+    collision = "top";
   }
 
-  function gameLoop(){
-    requestAnimationFrame(gameLoop);
-
-
-    //
-    background.vx = -3 
-    background.tilePosition.x += background.vx;
-    foreground.vx = -5 
-    foreground.tilePosition.x += foreground.vx; 
-
-
-    // Update token velocity
-    token.vx = -4;
-
-
-
-    // Apply velocity values to position
-    sprite.x += sprite.vx;
-    sprite.y += sprite.vy;
-    token.x += token.vx;
-    
-    renderer.render(stage);
+  if (sprite.x < container.x) {
+    sprite.x = container.x;
+    collision = "left";
   }
 
-  //The `keyboard` helper function
-function keyboard(keyCode) {
-  var key = {};
-  key.code = keyCode;
-  key.isDown = false;
-  key.isUp = true;
-  key.press = undefined;
-  key.release = undefined;
-  //The `downHandler`
-  key.downHandler = function(event) {
-    if (event.keyCode === key.code) {
-      if (key.isUp && key.press) key.press();
-      key.isDown = true;
-      key.isUp = false;
-    }
-    event.preventDefault();
-  };
-  //The `upHandler`
-  key.upHandler = function(event) {
-    if (event.keyCode === key.code) {
-      if (key.isDown && key.release) key.release();
-      key.isDown = false;
-      key.isUp = true;
-    }
-    event.preventDefault();
-  };
-  //Attach event listeners
-  window.addEventListener(
-    "keydown", key.downHandler.bind(key), false
-  );
-  window.addEventListener(
-    "keyup", key.upHandler.bind(key), false
-  );
-  return key;
+  if (sprite.x + sprite.width > container.x) {
+    sprite.x = container.x - sprite.width;
+    collision = "right";
+  }
+
+
+  if (sprite.y + sprite.height > container.y) {
+  sprite.y = container.height - sprite.height;
+  collision = "down";
+  }
+
+  return collision;
+}
+
+makeToken();
+
+function makeToken(){
+  var tokenArr = [];
+  setInterval(function(){
+    stage.addChild(token);
+  }, 1000);
 }
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,33 +1,94 @@
-  function gotStream(stream) {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    var audioContext = new AudioContext();
-    var analyser = audioContext.createAnalyser();
-    analyser.fftSize = 256;
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
 
-    // analyser.getFloatFrequencyData(dataArray);
-    // analyser.getByteFrequencyData(dataArray);
-    // for(var i=0; i < bufferLength; i++){
-    //   console.log(dataArray[i]);
-     // }
-
-      
+var MicrophoneStream = require('microphone-stream');
+ 
+getUserMedia({ video: false, audio: true }, function(err, stream) {
+  var micStream = new MicrophoneStream(stream);
+  
+  // get Buffers (Essentially a Uint8Array DataView of the same Float32 values) 
+  micStream.on('data', function(chunk) {
+    // Optionally convert the Buffer back into a Float32Array 
+    // (This actually just creates a new DataView - the underlying audio data is not copied or modified.) 
+    var raw = MicrophoneStream.toRaw(chunk) 
+    //... 
     
+    // note: if you set options.objectMode=true, the `data` event will output AudioBuffers instead of Buffers 
+   });
+  
+  // or pipe it to another stream 
+  micStream.pipe(/*...*/);
+  
+  // It also emits a format event with various details (frequency, channels, etc) 
+  micStream.on('format', function(format) {
+    console.log(format);
+  });
+  
+  // Stop when ready 
+  document.getElementById('my-stop-button').onclick = function() {
+    micStream.stop();
+  };
+});
 
-  //   // Create an AudioNode from the stream.
-    var mediaStreamSource = audioContext.createMediaStreamSource( stream );
-
-  //   // Connect it to the destination to hear yourself (or any other node for processing!)
-    mediaStreamSource.connect( audioContext.destination );
-  }
-  function error() {
-    console.log('error');
-  }
 
 
-// Access the microphone from the browser
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-  navigator.getUserMedia( {audio:true}, gotStream, error );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Access the microphone from the browser
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+
+// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+// var audioContext =  new AudioContext();
+// var bufferLength = null;
+// var dataArray;
+
+// function gotStream(stream) {
+//   var source = audioContext.createMediaStreamSource(stream);
+//   analyser = audioContext.createAnalyser();
+//   source.connect(analyser);
+//   analyser.fftSize = 256;
+//   bufferLength = analyser.frequencyBinCount;
+//   dataArray = new Uint8Array(bufferLength);
+//   analyser.getByteFrequencyData(dataArray);
+//   console.log("dataArray", dataArray);
+
+//   source.start();
+
+  
+  
+//   // for (var i = 0; i < bufferLength; i++) {
+//   //   console.log(dataArray[i]);
+//   // }
+// }
+
+// function error() {
+//   console.log('error');
+// }
+
+// function analyse() {
+
+// }
+
+// // (function(){setInterval(function(){
+// //   for (var i = 0; i < bufferLength; i++) {
+// //     console.log(dataArray[i]);
+// //   }
+// // }, 1000)})();
+
+navigator.getUserMedia( {audio:true}, gotStream(), error );
+
+
 
 
